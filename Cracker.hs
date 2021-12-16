02dbc6681b4b67b8450d41f2bbd9ba3c645b6094
Justin Chen <justinbrianchen@yahoo.com>
2021-12-15 22:18:45 -0500

    refactor reused mask constant

--- Cracker.hs
+++ Cracker.hs
@@ -2,6 +2,8 @@ module Cracker(calcChunkVal, randomNext, matches) where
 import Control.Monad
 import Data.Bits
 
+mask48Bit = shiftL 1 48 - 1
+
 calcChunkVal :: Int -> Int -> Int
 calcChunkVal x z = xsqv + xv + zsqv + zv 
     where xsqv = x * x * 0x4c1906
@@ -10,16 +12,14 @@ calcChunkVal x z = xsqv + xv + zsqv + zv
           zv   = z * 0x5f24f
 
 randomNext :: Int -> Int
-randomNext seed = (seed * 0x5DEECE66D  * 0xB ) .&. mask 
-    where mask = shiftL 1 48 - 1
+randomNext seed = (seed * 0x5DEECE66D  * 0xB ) .&. mask48Bit
 
 randomReverse :: Int -> Int
-randomReverse seed =  ((seed - 0xB ) * 246154705703781 ) .&. mask 
-    where mask = shiftL 1 48 - 1
+randomReverse seed =  ((seed - 0xB ) * 246154705703781 ) .&. mask48Bit
 
 matches :: Int -> Int -> Int
 matches chunk_val seed = do 
-    let random = ((seed + chunk_val) `xor`  0x5e434e432) .&. (shiftL 1 48 - 1)
+    let random = ((seed + chunk_val) `xor`  0x5e434e432) .&. mask48Bit
     let val = 0 
     let loop = do 
         let random = randomNext random
@@ -32,7 +32,7 @@ matches chunk_val seed = do
 
 filterEven :: Int -> Int -> Int 
 filterEven chunk_val seed = do 
-    let random = ((seed + chunk_val) `xor` 0x5e434e432) .&. (shiftL 1 48 - 1) 
+    let random = ((seed + chunk_val) `xor` 0x5e434e432) .&. mask48Bit
     let val = 0 
     let loop = do 
         let random = randomNext random 
@@ -40,4 +40,4 @@ filterEven chunk_val seed = do
         let val = bits `mod` 10
         when (bits - val +  9 < 0) loop
     loop
-    return (val `mod` 2) == 0 
+    return (val `mod` 2) == 0
