cddbca5844859a2a0038a953662d87fabd18b38b
Justin Chen <justinbrianchen@yahoo.com>
2021-12-22 02:08:47 -0800

    arg logic + fix file I/O + readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # seedCracker
+
+You can install dependencies, build and run using `stack run`.
+
+To record performance measurements, use `stack exec --` ..., eg:
+```
+stack exec -- Cracker-exe vals.txt parList 128 +RTS -N8 -s -ls
+```
--- app/Main.hs
+++ app/Main.hs
@@ -5,23 +5,23 @@ import System.Environment(getArgs)
 import Control.Monad(unless)
 import System.Exit(die)
 import Data.List(intercalate)
-import Data.Int (Int32)
+import Data.Ix(inRange)
 
 main :: IO ()
 main = do
     let types = ["seq", "parList", "parBuffer", "parMonad"]
     args <- getArgs
-    unless (length args ==0 || length args == 1 || (length args <= 3 && head args `elem` types ) ) $
-        die $ "Usage: <filename> (" ++ (intercalate "|" types) ++ ") <num-blocks> [naive]"
+    unless (inRange (1, 4) (length args) && args !! 1 `elem` types) $
+        die $ "Usage: <filename> (" ++ intercalate "|" types ++
+            ") <num-blocks> [naive]"
     let numBlocks = case args of
-            [_, num] -> read num
+            _ : _ : num : _ -> read num
             _ -> 128
     chunkCoords <- case args of
-            [filename, _] -> do
-                    text <- readFile filename
-                    let fileLines = map read  . lines $ text
-                    return $ map parseLine fileLines
-            _ -> return [(-1, -4), (15, -7), (-11, -4), (18, -2), (-3, 4), (-15, -10), (-16, 4), (-17, -3), (-9, -10), (-8, -13), (-19, -2), (-8, -14), (-9, -14), (-178, -99), (-91, -51), (-10, 7), (61, 61), (66, 66)]
+        filename : _ : _ -> do
+            text <- readFile filename
+            return $ map (((\[x, y] -> (x, y)) . map read) . words) (lines text)
+        _ -> return []
     let chunkVals = map calcChunkVal chunkCoords
     putStrLn "Calculated the following chunk values from the provided \
         \chunk coordinates:"
@@ -33,18 +33,15 @@ main = do
             [_, "parList", _, "naive"] ->
                 calcParListBlocksNaive numBlocks chunkVals lowerBits
             [_, "seq", _, "naive"] -> calcSeqNaive chunkVals lowerBits
-            [_, "parList"]  -> calcParListBlocks numBlocks chunkVals lowerBits
-            [_, "seq"] -> calcSeq chunkVals lowerBits
-            [_, "parBuffer"]  -> calcParBufferNaive chunkVals lowerBits
-            [_, "parMonad"] -> calcParMonadBlocks numBlocks chunkVals lowerBits
+            _ : "parList" : _  ->
+                calcParListBlocks numBlocks chunkVals lowerBits
+            _ : "seq" : _ -> calcSeq chunkVals lowerBits
+            _ : "parBuffer" : _  -> calcParBufferNaive chunkVals lowerBits
+            _ : "parMonad" : _ ->
+                calcParMonadBlocks numBlocks chunkVals lowerBits
             _ -> [0]
     putStrLn "Calculated the following valid 40-bit seeds:"
     print seeds
     let fullSeeds = concatMap expand48To64Random seeds
     putStrLn "Calculated the following valid 64-bit randomly generatable seeds:"
     print fullSeeds
-
-parseLine :: String -> (Int32, Int32)
-parseLine line = (n, head rest) 
-    where  [(n, rest)] = read line
-    
